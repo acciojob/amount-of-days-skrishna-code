2cc36d19122e8fe2e8a1846131878c2775fb31a6
skrishna-code <krishnasutar026@gmail.com>
2026-08-11 08:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-//your JS code here. If required.
-
-function daysInYear(year) {
+function daysOfAYear(year) {
     if (year % 400 === 0) {
         return 366;
-    }
-
-    if (year % 100 === 0) {
+    } else if (year % 100 === 0) {
+        return 365;
+    } else if (year % 4 === 0) {
+        return 366;
+    } else {
         return 365;
     }
-
-    if (year % 4 === 0) {
-        return 366;
-    }
-
-    return 365;
-}
 }
